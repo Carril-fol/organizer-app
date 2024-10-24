551f63c139859f25e3469ce4e07b79956b0fc749
Folco Carril <folco.carril@gmail.com>
2024-10-24 17:38:03 -0300

    feat: Login page added

---
+++ frontend/src/pages/auth/LoginPage.jsx
@@ -0,0 +1,10 @@
+import LoginForm from "../../components/forms/auth/LoginForm";
+
+const LoginPage = () => {
+    return (
+        <section className="flex justify-center items-center min-h-screen">
+            <LoginForm />
+        </section>
+    );
+}
+export default LoginPage;
