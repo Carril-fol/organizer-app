56f83c9c177546c4458da33891e2ed9689a0ddd3
Folco Carril <folco.carril@gmail.com>
2024-10-24 17:38:14 -0300

    feat: Register page added

---
+++ frontend/src/pages/auth/RegisterPage.jsx
@@ -0,0 +1,10 @@
+import RegisterForm from "../../components/forms/auth/RegisterForm";
+
+const RegisterPage = () => {
+    return (
+        <section className="flex justify-center items-center min-h-screen">
+            <RegisterForm />
+        </section>
+    );
+}
+export default RegisterPage;
