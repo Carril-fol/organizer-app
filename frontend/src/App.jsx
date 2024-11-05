a1b2138bc20538fcd7d8b02651a95b2a393825fc
Folco Carril <107985251+Carril-fol@users.noreply.github.com>
2024-11-05 03:21:58 -0600

    chore: Added a landingpage endpoint

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,7 @@
 import './styles/App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Header from "./components/common/Header";
-
+import LandingPage from './pages/core/LandingPage';
 import LoginPage from './pages/auth/LoginPage';
 import RegisterPage from './pages/auth/RegisterPage';
 import HomePage from './pages/dashboard/HomePage';
@@ -10,8 +9,8 @@ import HomePage from './pages/dashboard/HomePage';
 function App() {
   return (
     <BrowserRouter>
-      <Header />
       <Routes>
+        <Route path="/" element={<LandingPage />} />
         <Route path="auth/login" element={<LoginPage />} />
         <Route path="auth/register" element={<RegisterPage />} />
         <Route path="/dashboard" element={<HomePage />} />
