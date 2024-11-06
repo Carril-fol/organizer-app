ec5d00023101afa7fc9307932cbf0ddc14f33f48
Folco Carril <107985251+Carril-fol@users.noreply.github.com>
2024-11-06 03:38:39 -0600

    chore: Component modified

--- frontend/src/components/landing/ContainerTextMain.jsx
+++ frontend/src/components/landing/ContainerTextMain.jsx
@@ -1,17 +1,27 @@
 import { Button } from "@nextui-org/react";
+import { Link } from "react-router-dom";
+import Logo from "../common/LogoInWhite";
 
-const Jorge = () => {
+const ContainerTextMain = () => {
   return (
     <div>
-      <h2 className="text-4xl font-bold mb-4 text-white">Organiza tu vida con MoosePlanner</h2>
+      <div className="flex flex-col justify-center items-center gap-5">
+        <Logo
+          width={80}
+          height={80}
+        />
+        <h2 className="text-4xl font-bold mb-4 text-white">Organiza tu vida con MoosePlanner</h2>
+      </div>
       <p className="text-xl text-gray-200 mb-8">
         Simplifica tu día a día con carpetas y tareas personalizadas
       </p>
-      <Button size="lg" className="text-lg px-8 bg-white">
-        Comienza gratis
-      </Button>
+      <Link to="auth/login">
+        <Button size="lg" className="text-lg px-8 bg-white font-semibold">
+          Comienza gratis
+        </Button>
+      </Link>
     </div>
   );
 }
 
-export default Jorge;
+export default ContainerTextMain;
