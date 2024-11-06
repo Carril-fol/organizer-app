b38bb5e4cbe7878232a133ebfa6f71dac9cf318d
Folco Carril <107985251+Carril-fol@users.noreply.github.com>
2024-11-06 03:39:35 -0600

    chore: Login page modified

--- frontend/src/pages/auth/LoginPage.jsx
+++ frontend/src/pages/auth/LoginPage.jsx
@@ -1,10 +1,15 @@
 import LoginForm from "../../components/forms/auth/LoginForm";
+import Header from "../../components/common/Header";
 
 const LoginPage = () => {
-    return (
-        <section className="flex justify-center items-center min-h-screen">
-            <LoginForm />
-        </section>
-    );
+  return (
+    <main className="min-h-screen">
+      <Header />
+      <section className="flex justify-center items-center min-h-screen">
+        <LoginForm />
+      </section>
+    </main>
+  );
 }
+
 export default LoginPage;
