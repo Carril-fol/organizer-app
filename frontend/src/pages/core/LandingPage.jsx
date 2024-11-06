cf7422389111399a8c2b8c38047ed292f3026c79
Folco Carril <107985251+Carril-fol@users.noreply.github.com>
2024-11-06 03:38:51 -0600

    chore: Page modified

--- frontend/src/pages/core/LandingPage.jsx
+++ frontend/src/pages/core/LandingPage.jsx
@@ -1,18 +1,18 @@
-import Header from "../../components/landing/Header";
 import Footer from "../../components/common/Footer";
 import ContainerFeatureCards from "../../components/landing/ContainerFeatureCards";
 import ContainerTextMain from "../../components/landing/ContainerTextMain";
 
 const LandingPage = () => {
   return (
-    <div className="min-h-screen bg-gradient-to-b from-gray-900 to-black text-gray-100">
-      <Header />
-      <main className="container mx-auto px-4 py-16 text-center">
-        <ContainerTextMain />
-        <ContainerFeatureCards />
-        <Footer />
-      </main>
-    </div>
+    <main className="min-h-screen bg-gradient-to-b from-gray-900 to-black text-gray-100">
+      <div className="container mx-auto px-4 py-8">
+        <div className="container mx-auto px-4 py-16 text-center">
+          <ContainerTextMain />
+          <ContainerFeatureCards />
+          <Footer />
+        </div>
+      </div>
+    </main>
   );
 }
 export default LandingPage;
