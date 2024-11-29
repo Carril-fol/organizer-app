a1d4f2c63bb71631bccf96c146b42cc2e96bfe3e
Folco Carril <107985251+Carril-fol@users.noreply.github.com>
2024-11-28 22:35:31 -0300

    chore: Component removed

--- frontend/src/components/common/InputForm.jsx
+++
@@ -1,8 +0,0 @@
-import { Input } from "@nextui-org/react";
-
-const InputForm = ({ label, type }) => {
-    return (
-        <Input label={label} type={type} variant="underlined" />
-    );
-}
-export default InputForm;
