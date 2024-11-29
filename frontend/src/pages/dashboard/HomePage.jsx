f4704f7977f9c5ad22b3ee69864d796507762793
Folco Carril <107985251+Carril-fol@users.noreply.github.com>
2024-11-28 23:41:12 -0300

    chore: Added header in home page

--- frontend/src/pages/dashboard/HomePage.jsx
+++ frontend/src/pages/dashboard/HomePage.jsx
@@ -1,12 +1,16 @@
+import LoggedHeader from "../../components/common/LoggedHeader";
 import FolderTaskContainer from "../../components/dashboard/container/FolderTaskContainer";
 
 const HomePage = () => {
-    return(
-        <section className="w-screen h-screen flex justify-center items-center">
-            <div className="w-3/5">
-                <FolderTaskContainer />
-            </div>
-        </section>
-    );
+  return (
+    <main>
+      <LoggedHeader />
+      <section className="w-screen h-screen flex justify-center items-center">
+        <div className="w-3/5">
+          <FolderTaskContainer />
+        </div>
+      </section>
+    </main>
+  );
 }
 export default HomePage;
