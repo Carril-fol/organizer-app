a703aa05814a6738f33df8e3d9974a272e9bbeec
Folco Carril <107985251+Carril-fol@users.noreply.github.com>
2024-11-28 23:30:07 -0300

    feat: Added function to logout

--- frontend/src/components/common/LoggedNavbar.jsx
+++ frontend/src/components/common/LoggedNavbar.jsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import {
   Navbar,
   NavbarBrand,
@@ -9,15 +10,27 @@ import {
   DropdownItem,
   Button
 } from "@nextui-org/react";
+import {
+  LogOut,
+  UserRoundCog,
+  MenuIcon
+} from 'lucide-react';
 
-import { LogOut, UserRoundCog, MenuIcon } from 'lucide-react';
-import Logo from "./Logo";
+import LogoInBlack from "./LogoInBlack";
+import { logout } from "../../services/authServices";
 
 const LoggedNavbar = () => {
+  const navigate = useNavigate();
+
+  const handleSubmit = async () => {
+    await logout();
+    navigate("/auth/login")
+  }
+
   return (
     <Navbar>
       <NavbarBrand>
-        <Logo height={80} width={80} />
+        <LogoInBlack height={40} width={40} />
         <p className="font-bold text-inherit">Moose Planner</p>
       </NavbarBrand>
       <NavbarContent justify="end">
@@ -36,10 +49,10 @@ const LoggedNavbar = () => {
                 </span>
               </DropdownItem>
               <DropdownItem className="flex items-center text-black" color="danger">
-                <span className="flex items-center font-bold">
+                <button onClick={handleSubmit} className="flex items-center font-bold">
                   <LogOut className="mr-1" />
                   Logout
-                </span>
+                </button>
               </DropdownItem>
             </DropdownMenu>
           </Dropdown>
