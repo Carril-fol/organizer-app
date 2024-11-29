e1a593555844e5cf40cecb4a11b90068a5387d87
Folco Carril <107985251+Carril-fol@users.noreply.github.com>
2024-11-28 23:39:58 -0300

    chore: Added functions with the backend in the auth forms

--- frontend/src/components/forms/auth/LoginForm.jsx
+++ frontend/src/components/forms/auth/LoginForm.jsx
@@ -1,12 +1,34 @@
 import { useState } from "react";
-import { Button, Input, Link, Checkbox } from "@nextui-org/react";
+import { useNavigate } from "react-router-dom";
+import { Button, Input, Link } from "@nextui-org/react";
 import LogoInBlack from "../../common/LogoInBlack";
 
+import { login } from "../../../services/authServices";
+
+
 const LoginForm = () => {
-  const [isSelected, setIsSelected] = useState(false);
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+    try {
+      await login({ email, password });
+      navigate("/dashboard");
+    } catch (error) {
+      setError("Credenciales invalidas");
+    }
+  };
 
   return (
-    <form method="POST" className="min-w-fit px-5 py-5 rounded-xl translate-y-full animate-move-up transition-all duration-100 ease-out">
+    <form
+      method="POST"
+      className="min-w-fit px-5 py-5 rounded-xl translate-y-full animate-move-up transition-all duration-100 ease-out"
+      onSubmit={handleSubmit}
+    >
       <div className="flex max-w-full justify-center">
         <div className="min-w-80 flex justify-center">
           <div className="flex flex-col justify-center">
@@ -16,34 +38,60 @@ const LoginForm = () => {
         <div className="min-w-80">
           <div className="mx-5">
             <h2 className="text-2xl font-bold">Iniciar Sesión</h2>
-            <p className="text-sm text-gray-600">Por favor, ingresa tus credenciales para acceder a tu cuenta.</p>
+            <p className="text-sm text-gray-600">
+              Por favor, ingresa tus credenciales para acceder a tu cuenta.
+            </p>
+            {error && (
+              <div className="bg-red-100 text-red-700 p-2 rounded my-2">
+                {error}
+              </div>
+            )}
             <div className="py-2">
-              <Input label="Email" type="text" variant="underlined" className="mb-1" required />
+              <Input
+                label="Email"
+                type="text"
+                variant="underlined"
+                className="mb-1"
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
             <div className="py-2">
-              <Input label="Password" type="password" variant="underlined" className="mb-1" required />
+              <Input
+                label="Password"
+                type="password"
+                variant="underlined"
+                className="mb-1"
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
               <Link className="text-sm text-blue-500 hover:underline">
                 ¿Olvidaste tu contraseña?
               </Link>
             </div>
-            <div className="flex items-center my-3">
-              <Checkbox isSelected={isSelected} onValueChange={setIsSelected}>
-                Recordar mi sesión
-              </Checkbox>
-            </div>
             <div className="py-4 flex flex-col justify-center">
-              <Button size="md" color="default" className="bg-black text-white font-semibold">
+              <Button
+                size="md"
+                color="default"
+                className="bg-black text-white font-semibold"
+                type="submit"
+              >
                 Iniciar Sesión
               </Button>
             </div>
             <p className="text-sm text-gray-600 mt-4">
-              ¿No tienes una cuenta? <Link className="text-blue-500 hover:underline">Regístrate aquí</Link>.
+              ¿No tienes una cuenta?{' '}
+              <a href="register" className="text-blue-500 hover:underline">
+                Registrate
+              </a>
             </p>
           </div>
         </div>
       </div>
     </form>
   );
-}
+};
 
 export default LoginForm;
--- frontend/src/components/forms/auth/RegisterForm.jsx
+++ frontend/src/components/forms/auth/RegisterForm.jsx
@@ -1,38 +1,106 @@
-import { Button } from "@nextui-org/react";
-import InputForm from "../../common/InputForm";
-import Logo from "../../common/Logo";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Button, Input } from "@nextui-org/react";
+import LogoInBlack from "../../common/LogoInBlack";
+
+import { register } from "../../../services/authServices";
 
 const RegisterForm = () => {
+  const navigate = useNavigate();
+  const [first_name, setFirstName] = useState('');
+  const [last_name, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirm_password, setConfirmPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+    try {
+      await register({ first_name, last_name, email, password, confirm_password });
+      navigate("/dashboard");
+    } catch (error) {
+      setError("Ocurrio un error al momento de la creacion");
+    }
+  }
+
   return (
-    <form method="post" className="w-1/5 rounded-xl shadow-xl translate-y-full animate-move-up transition-all duration-1000 ease-out">
-      <div className="py-3 flex-row px-5 justify-center items-center">
-        <div>
-          <div className="flex flex-col justify-center items-center">
-            <Logo width={80} height={80} alt="Logo" />
-            <h2 className="font-semibold text-xl py-3">
-              Create a Moose Planner account
-            </h2>
+    <form method="POST" className="min-w-fit px-5 py-5 rounded-xl translate-y-full animate-move-up transition-all duration-100 ease-out" onSubmit={handleSubmit}>
+      <div className="flex max-w-full justify-center">
+        <div className="min-w-80 flex justify-center items-center flex-col px-5">
+          <div className="flex flex-col items-center">
+            <LogoInBlack width={60} height={60} alt="Logo" />
+            <h2 className="text-2xl font-bold mt-4 text-center">Bienvenido a MoosePlanner</h2>
+            <p className="text-sm text-gray-600 mt-2 text-center">
+              Planifica tu día con facilidad. Crea tu cuenta para comenzar.
+            </p>
           </div>
         </div>
-        <div className="py-2">
-          <InputForm label="First name" type="text" />
-        </div>
-        <div className="py-2">
-          <InputForm label="Last name" type="text" />
-        </div>
-        <div className="py-2">
-          <InputForm label="Email" type="text" />
-        </div>
-        <div className="py-2">
-          <InputForm label="Password" type="password" />
-        </div>
-        <div className="py-2">
-          <InputForm label="Confirm password" type="password" />
-        </div>
-        <div className="py-4 flex justify-center">
-          <Button size="md" color="default" variant="ghost" className="font-bold">
-            Sign up
-          </Button>
+        <div className="min-w-80">
+          <div className="mx-5">
+            <div className="py-2">
+              <Input
+                label="Nombre"
+                type="text"
+                variant="underlined"
+                className="mb-1"
+                isRequired
+                value={first_name}
+                onChange={(e) => setFirstName(e.target.value)}
+              />
+            </div>
+            <div className="py-2">
+              <Input
+                label="Apellido"
+                type="text"
+                variant="underlined"
+                className="mb-1"
+                isRequired
+                value={last_name}
+                onChange={(e) => setLastName(e.target.value)}
+              />
+            </div>
+            <div className="py-2">
+              <Input
+                label="Email"
+                type="text"
+                variant="underlined"
+                className="mb-1"
+                isRequired
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="py-2">
+              <Input
+                label="Contraseña"
+                type="password"
+                variant="underlined"
+                className="mb-1"
+                isRequired
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className="py-2">
+              <Input
+                label="Confirmar contraseña"
+                type="password"
+                variant="underlined"
+                className="mb-1"
+                isRequired
+                value={confirm_password}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+            </div>
+            <div className="py-4 flex flex-col justify-center">
+              <Button size="md" color="default" className="bg-black text-white font-semibold" type="submit">
+                Registrarse
+              </Button>
+            </div>
+            <p>¿Tiene una cuenta ya registrada? <a href="login" className="text-blue-500 hover:underline">Inicia sesión</a></p>
+          </div>
         </div>
       </div>
     </form>
