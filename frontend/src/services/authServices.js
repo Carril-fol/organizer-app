79015f5bc0b8a92b63f9c7b2d5eb9a9ee30c7cc0
Folco Carril <107985251+Carril-fol@users.noreply.github.com>
2024-12-02 02:38:57 -0300

    feat: Added file with function to manage auth from users

--- frontend/src/services/authServices.js
+++ frontend/src/services/authServices.js
@@ -1,14 +1,12 @@
 import API from "./api";
 
-let header = { "Content-Type": "application/json" };
-
 const urlAuth = (endpoint) => {
   return `/users/api/v1/${endpoint}`;
 };
 
 export const register = async (userData) => {
   try {
-    await API.post(urlAuth("register"), userData, header);
+    await API.post(urlAuth("register"), userData);
   } catch (error) {
     const errorMessage = "Verifique los datos introducidos";
     throw new Error(errorMessage);
@@ -17,7 +15,7 @@ export const register = async (userData) => {
 
 export const login = async (userData) => {
   try {
-    await API.post(urlAuth("login"), userData, { withCredentials: true }, header);
+    await API.post(urlAuth("login"), userData, { withCredentials: true });
   } catch (error) {
     const errorMessage = "Crendenciales invalidas";
     throw new Error(errorMessage);
@@ -26,10 +24,18 @@ export const login = async (userData) => {
 
 export const logout = async () => {
   try {
-    const response = await API.post(urlAuth("logout"), {}, { withCredentials: true });
-    console.log(response.data);
+    await API.post(urlAuth("logout"), {}, { withCredentials: true });
   } catch (error) {
     console.error("Error en logout:", error.response?.data || error.message);
     throw new Error(error.response?.data?.msg || "Error desconocido");
   }
 };
+
+export const refreshTokens = () => {
+  try {
+    API.post(urlAuth("refresh"));
+  } catch (error) {
+    console.error("Error en el refresh:", error.response?.data || error.message);
+    throw new Error(error.response?.data?.msg || "Error desconocido");
+  }
+}
