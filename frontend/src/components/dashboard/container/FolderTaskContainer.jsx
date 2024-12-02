8543d793986e0edddad573c2184eb4a0c6d724a6
Folco Carril <107985251+Carril-fol@users.noreply.github.com>
2024-12-02 01:51:37 -0300

    feat: Added a component that works as a container for folders and tasks

--- frontend/src/components/dashboard/container/FolderTaskContainer.jsx
+++ frontend/src/components/dashboard/container/FolderTaskContainer.jsx
@@ -1,21 +1,36 @@
-import { Card, CardHeader, CardBody } from "@nextui-org/react";
+import { useState } from "react";
 
+import { detailFolder } from "../../../services/folderServices";
 import FolderManager from "../managers/FolderManager";
 import TaskManager from "../managers/TaskManager";
 
 const FolderTaskContainer = () => {
+  const [tasks, setTasks] = useState([]);
+  const [selectedFolder, setSelectedFolder] = useState(null);
+
+  const handleFolderSelect = async (folderId) => {
+    setSelectedFolder(folderId);
+    try {
+      const response = await detailFolder(folderId);
+      setTasks(response.data.tasks);
+    } catch (error) {
+      throw new Error("Error al obtener tareas:", error);
+    }
+  };
+
   return (
-    <Card className="w-full max-w-4xl mx-auto">
-      <CardHeader className="flex justify-center">
-        <h1 className="text-2xl font-bold text-center">Gestor de Carpetas y Tareas</h1>
-      </CardHeader>
-      <CardBody>
-        <div className="flex space-x-11 justify-evenly">
-          <FolderManager />
-          <TaskManager />
+    <main className="w-screen h-screen flex justify-center">
+      <div className="flex w-full">
+        <aside className="min-w-fit p-4 border-r-2 border-black">
+          <FolderManager onFolderSelect={handleFolderSelect} />
+        </aside>
+        <div className="flex flex-col w-3/4 p-4">
+          <div className="flex">
+            <TaskManager tasks={tasks} />
+          </div>
         </div>
-      </CardBody>
-    </Card>
+      </div>
+    </main>
   );
 }
 export default FolderTaskContainer;
