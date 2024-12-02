97d31d389d2ba84772a0928bcf36df2da762bb50
Folco Carril <107985251+Carril-fol@users.noreply.github.com>
2024-12-02 01:53:53 -0300

    feat: Added a component that contains the list of folders and different functions

--- frontend/src/components/dashboard/managers/FolderManager.jsx
+++ frontend/src/components/dashboard/managers/FolderManager.jsx
@@ -1,31 +1,53 @@
-import { Input, Button } from "@nextui-org/react";
-import { Folder, Plus } from "lucide-react";
+import { useState, useEffect } from "react";
+import { Folder } from "lucide-react";
 
-const FolderManager = () => {
+import FolderList from "../lists/FolderLists";
+import CreateFolderForm from "../../forms/folders/CreateFolderForm";
+import { getFolders, createFolder } from "../../../services/folderServices";
+
+const FolderManager = ({ onFolderSelect  }) => {
+  const [folders, setFolders] = useState([]);
+  const [error, setError] = useState("");
+
+  const fetchFolders = async () => {
+    try {
+      const response = await getFolders();
+      setFolders(response.folders);
+    } catch (error) {
+      setError("Error al obtener carpetas");
+    }
+  };
+
+  const handleCreateFolder = async (name_folder) => {
+    try {
+      await createFolder({ name_folder });
+      fetchFolders();
+    } catch (error) {
+      setError("Error al crear carpeta");
+    }
+  };
+
+  useEffect(() => {
+    fetchFolders();
+  }, []);
+
   return (
-    <div className="w-full md:w-1/3">
-      <h2 className="text-lg font-semibold mb-2">Carpetas</h2>
-      <div className="flex mb-4">
-        <Input
-          type="text"
-          placeholder="Nueva carpeta"
-          className="mr-2"
-        />
-        <Button className="flex items-center bg-black text-white font-bold">
-          <span className="flex text-white justify-center items-center">
-            <Plus className="h-4 w-4 mr-2" />
-            Añadir
-          </span>
-        </Button>
+    <div>
+      <div className="flex w-full border-b-2 pb-4 mb-5 space-x-2 items-center">
+        <Folder />
+        <h2 className="text-lg font-semibold">
+          Carpetas
+        </h2>
       </div>
-      <div className="space-y-2">
-        <Button variant="ghost" className="w-full justify-start">
-          <Folder className="h-4 w-4 mr-2">
-            nombre
-          </Folder>
-        </Button>
-      </div>
+      <CreateFolderForm onCreate={handleCreateFolder} />
+      {error && <p className="text-red-500">{error}</p>}
+      <FolderList
+        folders={folders}
+        fetchFolders={fetchFolders}
+        onFolderSelect={onFolderSelect}
+      />
     </div>
   );
-}
+};
+
 export default FolderManager;
