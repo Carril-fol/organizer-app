bb01f1ae13aa6622da028432f2930a626b75e363
Folco Carril <107985251+Carril-fol@users.noreply.github.com>
2024-12-02 01:55:25 -0300

    feat: A component has been added that works as a form that allows the deletion of folders

---
+++ frontend/src/components/forms/folders/DeleteFolderForm.jsx
@@ -0,0 +1,71 @@
+import { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import { Folder, Trash2, X } from "lucide-react";
+import {
+  Button,
+  Input,
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  useDisclosure,
+} from "@nextui-org/react";
+
+const DeleteFolderForm = ({ folder, onDelete }) => {
+  const [name_folder, setFolderName] = useState(folder.name_folder);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  useEffect(() => {
+    setFolderName(folder.name_folder);
+  }, [folder]);
+
+  const handleSubmitDelete = async (e) => {
+    e.preventDefault();
+    await onDelete(folder._id);
+    onClose();
+  };
+
+  return (
+    <>
+      <Button
+        className="bg-transparent max-w-fit max-h-fit flex items-center justify-center"
+        onClick={onOpen}
+      >
+        <Trash2 className="w-4" />
+      </Button>
+      <Modal isOpen={isOpen} onOpenChange={onClose}>
+        <ModalContent>
+          <ModalHeader>
+            <div className="flex w-full border-b-2 pb-4 mb-5 space-x-2 items-center">
+              <Folder />
+              <h2>Eliminar carpeta</h2>
+            </div>
+          </ModalHeader>
+          <ModalBody>
+            <form onSubmit={handleSubmitDelete}>
+              <Input isDisabled value={name_folder} />
+              <div className="flex justify-end gap-2 mt-4">
+                <Button variant="ghost" onClick={onClose}>
+                  <X />
+                </Button>
+                <Button type="submit" variant="solid" color="danger">
+                  <Trash2 />
+                </Button>
+              </div>
+            </form>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+DeleteFolderForm.propTypes = {
+  folder: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    name_folder: PropTypes.string.isRequired,
+  }).isRequired,
+  onDelete: PropTypes.func.isRequired,
+};
+
+export default DeleteFolderForm;
