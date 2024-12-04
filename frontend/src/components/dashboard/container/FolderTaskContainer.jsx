908ad4f8e6e0d73f02900ff0fa6860c2ef8e38bc
Folco Carril <107985251+Carril-fol@users.noreply.github.com>
2024-12-04 05:07:23 -0300

    chore: Logic to handle selected folders moved

--- frontend/src/components/dashboard/container/FolderTaskContainer.jsx
+++ frontend/src/components/dashboard/container/FolderTaskContainer.jsx
@@ -1,36 +1,20 @@
 import { useState } from "react";
 
-import { detailFolder } from "../../../services/folderServices";
 import FolderManager from "../managers/FolderManager";
 import TaskManager from "../managers/TaskManager";
 
 const FolderTaskContainer = () => {
-  const [tasks, setTasks] = useState([]);
-  const [selectedFolder, setSelectedFolder] = useState(null);
-
-  const handleFolderSelect = async (folderId) => {
-    setSelectedFolder(folderId);
-    try {
-      const response = await detailFolder(folderId);
-      setTasks(response.data.tasks);
-    } catch (error) {
-      throw new Error("Error al obtener tareas:", error);
-    }
-  };
+  const [selectedFolder, setSelectedFolder] = useState("");
 
   return (
-    <main className="w-screen h-screen flex justify-center">
-      <div className="flex w-full">
-        <aside className="min-w-fit p-4 border-r-2 border-black">
-          <FolderManager onFolderSelect={handleFolderSelect} />
-        </aside>
-        <div className="flex flex-col w-3/4 p-4">
-          <div className="flex">
-            <TaskManager tasks={tasks} />
-          </div>
-        </div>
+    <div className="w-screen h-screen flex">
+      <aside className="min-w-[250px] p-4 border-r-2 border-black">
+        <FolderManager onFolderSelect={setSelectedFolder} />
+      </aside>
+      <div className="flex-grow p-4">
+        <TaskManager folderId={selectedFolder} />
       </div>
-    </main>
+    </div>
   );
 }
 export default FolderTaskContainer;
