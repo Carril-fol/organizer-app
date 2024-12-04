d815348d3376c0926ad4f73d400be9dd9e5ba812
Folco Carril <107985251+Carril-fol@users.noreply.github.com>
2024-12-04 05:06:51 -0300

    chore: Interceptor added in API

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,7 @@
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import { refreshTokens } from "./authServices";
+
 
 const API = axios.create(
   {
@@ -8,6 +11,26 @@ const API = axios.create(
       'Content-Type': 'application/json',
     },
   },
+);
+
+
+API.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    if (error.response && error.response.status === 401) {
+      try {
+        await refreshTokens();
+        const originalRequest = error.config;
+        return API(originalRequest);
+      } catch (refreshError) {
+        console.error("Error al refrescar el token:", refreshError);
+        return Promise.reject(refreshError);
+      }
+    }
+    return Promise.reject(error);
+  }
 );
 
 export default API;
