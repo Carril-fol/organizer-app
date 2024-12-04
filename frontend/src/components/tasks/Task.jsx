e176df1e66f06f5cc74c17cadf5c22994b9a8799
Folco Carril <107985251+Carril-fol@users.noreply.github.com>
2024-12-04 05:13:34 -0300

    feat: Added a component "Task" to manage the tasks from the user

---
+++ frontend/src/components/tasks/Task.jsx
@@ -0,0 +1,18 @@
+import TaskItem from "./TaskItem";
+
+const Task = ({ tasks, fetchTasks }) => {
+  if (tasks.length === 0) {
+    return <p>No hay tareas en esta carpeta.</p>;
+  }
+
+  return (
+    <div className="space-y-4">
+      {tasks.map((task) => (
+        <TaskItem key={task._id} task={task} fetchTasks={fetchTasks}/>
+      ))}
+    </div>
+  );
+};
+
+export default Task;
+
