f55ce740e47c4bb3d6627d17f4343df9df29be43
Folco Carril <107985251+Carril-fol@users.noreply.github.com>
2024-12-04 05:07:56 -0300

    feat: Added function in component to fetch the tasks from user

--- frontend/src/components/dashboard/lists/TaskList.jsx
+++ frontend/src/components/dashboard/lists/TaskList.jsx
@@ -1,35 +1,53 @@
 import PropTypes from "prop-types";
-import { ClipboardList } from "lucide-react";
+import { useEffect, useState } from "react";
+import { ClipboardList } from 'lucide-react';
 
+import Task from "../../tasks/Task";
 import CreateTaskForm from "../../forms/tasks/CreateTaskForm";
+import { getAllTasksFromFolder } from "../../../services/tasksServices";
 
-const TaskList = ({ tasks }) => {
+const TaskList = ({ folderId }) => {
+  const [tasks, setTasks] = useState([]);
+  const hasTasks = tasks.length > 0;
+  const isScrollable = tasks.length > 5;
+
+  const fetchTasks = async () => {
+    try {
+      const response = await getAllTasksFromFolder(folderId);
+      setTasks(response.tasks);
+    } catch (error) {
+      throw new Error("Error al obtener las tareas de la tarea", error);
+    }
+  }
+
+  useEffect(() => {
+    if (folderId) {
+      fetchTasks();
+    }
+  }, [folderId]);
+
   return (
-    <div className="min-w-full">
+    <div className="w-full max-w-full">
       <div className="flex border-b-2 pb-4 mb-5 space-x-2 items-center">
         <ClipboardList />
-        <h2 className="text-lg font-semibold">
-          Lista de Tareas
-        </h2>
+        <h2 className="text-lg font-semibold">Lista de Tareas</h2>
       </div>
-      <CreateTaskForm />
-      <div>
-        {tasks.length > 0 ? (
-          tasks.map((task) => (
-            <div key={task._id} className="p-2 border rounded">
-              {task.name}
-            </div>
-          ))
+      <CreateTaskForm folderId={folderId} onTaskCreated={fetchTasks} />
+      {
+        hasTasks ? (
+          <div className={`flex flex-col gap-5 ${isScrollable ? "overflow-y-auto max-h-[calc(100vh-200px)]" : ""}`}>
+            <Task tasks={tasks} fetchTasks={fetchTasks} />
+          </div>
         ) : (
           <p>No hay tareas disponibles.</p>
-        )}
-      </div>
+        )
+      }
     </div>
   );
 };
 
 TaskList.propTypes = {
-  tasks: PropTypes.arrayOf(PropTypes.object).isRequired,
-}
+  folderId: PropTypes.string.isRequired,
+};
 
 export default TaskList;
