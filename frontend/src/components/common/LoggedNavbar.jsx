cfbb77c03bd9c3ad9ca1ada9c3b4d60194dadf78
Folco Carril <107985251+Carril-fol@users.noreply.github.com>
2024-12-04 23:50:01 -0300

    chore: Modified a navbar

--- frontend/src/components/common/LoggedNavbar.jsx
+++ frontend/src/components/common/LoggedNavbar.jsx
@@ -1,20 +1,6 @@
 import { useNavigate } from "react-router-dom";
-import {
-  Navbar,
-  NavbarBrand,
-  NavbarContent,
-  NavbarItem,
-  Dropdown,
-  DropdownTrigger,
-  DropdownMenu,
-  DropdownItem,
-  Button
-} from "@nextui-org/react";
-import {
-  LogOut,
-  UserRoundCog,
-  MenuIcon
-} from 'lucide-react';
+import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Button } from "@nextui-org/react";
+import { LogOut, UserRoundCog, MenuIcon } from 'lucide-react';
 
 import LogoInBlack from "./LogoInBlack";
 import { logout } from "../../services/authServices";
@@ -24,41 +10,33 @@ const LoggedNavbar = () => {
 
   const handleSubmit = async () => {
     await logout();
-    navigate("/auth/login")
-  }
+    navigate("/auth/login");
+  };
 
   return (
-    <Navbar>
-      <NavbarBrand>
+    <header className="mt-2 flex w-screen justify-between items-center px-4 py-2">
+      <div className="flex items-center">
         <LogoInBlack height={40} width={40} />
         <p className="font-bold text-inherit">Moose Planner</p>
-      </NavbarBrand>
-      <NavbarContent justify="end">
-        <NavbarItem>
-          <Dropdown>
-            <DropdownTrigger>
-              <Button variant="light">
-                <MenuIcon />
+      </div>
+      <div className="max-w-fit">
+        <Dropdown>
+          <DropdownTrigger>
+            <Button className="bg-transparent">
+              <MenuIcon />
+            </Button>
+          </DropdownTrigger>
+          <DropdownMenu>
+            <DropdownItem key="delete" className="text-danger" color="danger">
+              <Button onClick={handleSubmit} className="flex items-center font-bold text-gray-800 transition-colors bg-transparent">
+                <LogOut className="mr-1" />
               </Button>
-            </DropdownTrigger>
-            <DropdownMenu>
-              <DropdownItem key="setting" className="flex items-center text-black">
-                <span className="flex">
-                  <UserRoundCog className="mr-1" />
-                  Settings
-                </span>
-              </DropdownItem>
-              <DropdownItem className="flex items-center text-black" color="danger">
-                <button onClick={handleSubmit} className="flex items-center font-bold">
-                  <LogOut className="mr-1" />
-                  Logout
-                </button>
-              </DropdownItem>
-            </DropdownMenu>
-          </Dropdown>
-        </NavbarItem>
-      </NavbarContent>
-    </Navbar>
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+      </div>
+    </header>
   );
-}
+};
+
 export default LoggedNavbar;
