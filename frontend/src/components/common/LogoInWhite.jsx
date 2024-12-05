963fa74a23ad7019171a9621bf041f50e3a8d8cd
Folco Carril <107985251+Carril-fol@users.noreply.github.com>
2024-12-04 23:50:28 -0300

    chore: Modified logo import

--- frontend/src/components/common/LogoInWhite.jsx
+++ frontend/src/components/common/LogoInWhite.jsx
@@ -1,5 +1,5 @@
 import { Image } from "@nextui-org/react";
-import logo from "/images/logo-black.webp";
+import logo from "/images/logo-white.webp";
 
 const LogoInWhite = ({ width, height }) => {
   return (
