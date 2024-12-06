34beb21d41c58e55c05030cea91f5016ef7e88ac
Folco Carril <107985251+Carril-fol@users.noreply.github.com>
2024-12-06 17:56:29 -0300

    chore: Validation added in component

--- frontend/src/components/dashboard/container/FolderTaskContainer.jsx
+++ frontend/src/components/dashboard/container/FolderTaskContainer.jsx
@@ -7,14 +7,20 @@ const FolderTaskContainer = () => {
   const [selectedFolder, setSelectedFolder] = useState("");
 
   return (
-    <div className="w-screen h-screen flex">
-      <aside className="min-w-[250px] p-4 border-r-2 border-black">
-        <FolderManager onFolderSelect={setSelectedFolder} />
-      </aside>
-      <div className="flex-grow p-4">
-        <TaskManager folderId={selectedFolder} />
+    <section>
+      <div className="w-screen h-screen flex">
+        <aside className="min-w-[250px] p-4 border-r-2 border-black">
+          <FolderManager onFolderSelect={setSelectedFolder} />
+        </aside>
+        <div className="flex-grow p-4">
+          {selectedFolder ? (
+            <TaskManager folderId={selectedFolder} />
+          ) : (
+            <p className="text-gray-500">Por favor, selecciona una carpeta para ver sus tareas.</p>
+          )}
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
 export default FolderTaskContainer;
