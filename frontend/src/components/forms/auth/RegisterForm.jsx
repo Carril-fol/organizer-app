9864a838ace9623f55d60d4ad44609b7c5c494fb
Folco Carril <107985251+Carril-fol@users.noreply.github.com>
2024-12-08 19:11:03 -0300

    chore: Moved logic to register

--- frontend/src/components/forms/auth/RegisterForm.jsx
+++ frontend/src/components/forms/auth/RegisterForm.jsx
@@ -1,29 +1,22 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
 import { Button, Input } from "@nextui-org/react";
 import LogoInBlack from "../../common/LogoInBlack";
-
-import { register } from "../../../services/authServices";
+import useRegisterForm from "../../../hooks/auth/useRegisterForm";
 
 const RegisterForm = () => {
-  const navigate = useNavigate();
-  const [first_name, setFirstName] = useState('');
-  const [last_name, setLastName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirm_password, setConfirmPassword] = useState('');
-  const [error, setError] = useState('');
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setError("");
-    try {
-      await register({ first_name, last_name, email, password, confirm_password });
-      navigate("/dashboard");
-    } catch (error) {
-      setError("Ocurrio un error al momento de la creacion");
-    }
-  }
+  const {
+    first_name,
+    setFirstName,
+    last_name,
+    setLastName,
+    email,
+    setEmail,
+    password,
+    setPassword,
+    confirm_password,
+    setConfirmPassword,
+    error,
+    handleSubmit,
+  } = useRegisterForm();
 
   return (
     <form method="POST" className="min-w-fit px-5 py-5 rounded-xl translate-y-full animate-move-up transition-all duration-100 ease-out" onSubmit={handleSubmit}>
@@ -31,7 +24,9 @@ const RegisterForm = () => {
         <div className="min-w-80 flex justify-center items-center flex-col px-5">
           <div className="flex flex-col items-center">
             <LogoInBlack width={60} height={60} alt="Logo" />
-            <h2 className="text-2xl font-bold mt-4 text-center">Bienvenido a MoosePlanner</h2>
+            <h2 className="text-2xl font-bold mt-4 text-center">
+              Bienvenido a MoosePlanner
+            </h2>
             <p className="text-sm text-gray-600 mt-2 text-center">
               Planifica tu día con facilidad. Crea tu cuenta para comenzar.
             </p>
@@ -39,12 +34,14 @@ const RegisterForm = () => {
         </div>
         <div className="min-w-80">
           <div className="mx-5">
+            {error && (
+              <div className="bg-red-100 text-red-700 p-2 rounded my-2">{error}</div>
+            )}
             <div className="py-2">
               <Input
                 label="Nombre"
                 type="text"
                 variant="underlined"
-                className="mb-1"
                 isRequired
                 value={first_name}
                 onChange={(e) => setFirstName(e.target.value)}
@@ -55,7 +52,6 @@ const RegisterForm = () => {
                 label="Apellido"
                 type="text"
                 variant="underlined"
-                className="mb-1"
                 isRequired
                 value={last_name}
                 onChange={(e) => setLastName(e.target.value)}
@@ -66,7 +62,6 @@ const RegisterForm = () => {
                 label="Email"
                 type="text"
                 variant="underlined"
-                className="mb-1"
                 isRequired
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
@@ -77,7 +72,6 @@ const RegisterForm = () => {
                 label="Contraseña"
                 type="password"
                 variant="underlined"
-                className="mb-1"
                 isRequired
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
@@ -88,22 +82,32 @@ const RegisterForm = () => {
                 label="Confirmar contraseña"
                 type="password"
                 variant="underlined"
-                className="mb-1"
                 isRequired
                 value={confirm_password}
                 onChange={(e) => setConfirmPassword(e.target.value)}
               />
             </div>
             <div className="py-4 flex flex-col justify-center">
-              <Button size="md" color="default" className="bg-black text-white font-semibold" type="submit">
+              <Button
+                size="md"
+                color="default"
+                className="bg-black text-white font-semibold"
+                type="submit"
+              >
                 Registrarse
               </Button>
             </div>
-            <p>¿Tiene una cuenta ya registrada? <a href="login" className="text-blue-500 hover:underline">Inicia sesión</a></p>
+            <p>
+              ¿Ya tienes una cuenta?{" "}
+              <a href="login" className="text-blue-500 hover:underline">
+                Inicia sesión
+              </a>
+            </p>
           </div>
         </div>
       </div>
     </form>
   );
-}
+};
+
 export default RegisterForm;
