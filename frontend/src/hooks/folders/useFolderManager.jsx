f7522104dce0e3510ad182ef41422700ca489150
Folco Carril <107985251+Carril-fol@users.noreply.github.com>
2024-12-11 01:43:35 -0300

    feat: Added hook in FolerManager to separete the logic and the component

---
+++ frontend/src/hooks/folders/useFolderManager.jsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from "react";
+import { toast } from "react-toastify";
+import { getFolders, createFolder, detailFolder } from "../../services/folderServices";
+
+const useFolderManager = () => {
+  const [folders, setFolders] = useState([]);
+
+  const fetchFolders = async () => {
+    try {
+      const response = await getFolders();
+      setFolders(response.folders);
+    } catch (error) {
+      toast.error("Error al obtener carpetas");
+    }
+  };
+
+  const handleCreateFolder = async (name_folder) => {
+    try {
+      await createFolder({ name_folder });
+      fetchFolders();
+    } catch (error) {
+      toast.error("Error al crear carpeta");
+    }
+  };
+
+  const handleFolderSelect = async (folderId, onFolderSelect) => {
+    onFolderSelect(folderId);
+    try {
+      const response = await detailFolder(folderId);
+      return response.data;
+    } catch (error) {
+      toast.error("Error al obtener los detalles de la carpeta.");
+    }
+  };
+
+  useEffect(() => {
+    fetchFolders();
+  }, []);
+
+  return {
+    folders,
+    fetchFolders,
+    handleCreateFolder,
+    handleFolderSelect,
+  };
+};
+
+export default useFolderManager;
