9ec433b735c41d8e429e3bf7f8e6f0137b0afa00
Folco Carril <107985251+Carril-fol@users.noreply.github.com>
2024-12-11 02:10:16 -0300

    feat: Sepate logic to show a list from tasks

--- frontend/src/components/tasks/TaskList.jsx
+++ frontend/src/components/tasks/TaskList.jsx
@@ -1,19 +1,8 @@
-import PropTypes from "prop-types";
-import { ClipboardList } from "lucide-react";
 import Task from "./Task";
-import CreateTaskForm from "../forms/tasks/CreateTaskForm";
-import useTaskList from "../../hooks/tasks/useTaskList";
 
-const TaskList = ({ folderId }) => {
-  const { tasks, hasTasks, isScrollable, isLoading, error, fetchTasks } = useTaskList(folderId);
-
+const TaskList = ({ isScrollable, isLoading, error, hasTasks, tasks, fetchTasks }) => {
   return (
-    <div className="w-full max-w-full">
-      <div className="flex border-b-2 pb-4 mb-5 space-x-2 items-center">
-        <ClipboardList />
-        <h2 className="text-lg font-semibold">Lista de Tareas</h2>
-      </div>
-      <CreateTaskForm folderId={folderId} onTaskCreated={fetchTasks} />
+    <div>
       {isLoading && <p>Cargando tareas...</p>}
       {error && <p className="text-red-600">{error}</p>}
       {!isLoading && !error && (
@@ -27,10 +16,6 @@ const TaskList = ({ folderId }) => {
       )}
     </div>
   );
-};
-
-TaskList.propTypes = {
-  folderId: PropTypes.string.isRequired,
-};
+}
 
 export default TaskList;
