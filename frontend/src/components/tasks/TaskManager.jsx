2f61b2749854813c53216fa87f54310de9a9284b
Folco Carril <107985251+Carril-fol@users.noreply.github.com>
2024-12-11 02:11:16 -0300

    feat: Added a integration with a hook to seperate the logic from the component and TaskList to obtain the tasks

--- frontend/src/components/tasks/TaskManager.jsx
+++ frontend/src/components/tasks/TaskManager.jsx
@@ -1,15 +1,33 @@
 import PropTypes from "prop-types";
+import { ClipboardList } from "lucide-react";
 
 import TaskList from "./TaskList";
+import CreateTaskForm from "../tasks/forms/CreateTaskForm";
+import useTaskList from "../../hooks/tasks/useTaskList";
 
 const TaskManager = ({ folderId }) => {
+  const { tasks, hasTasks, isScrollable, isLoading, error, fetchTasks } = useTaskList(folderId);
 
   return (
-    <TaskList folderId={folderId} />
+    <div className="w-full max-w-full">
+      <div className="flex border-b-2 pb-4 mb-5 space-x-2 items-center">
+        <ClipboardList />
+        <h2 className="text-lg font-semibold">Lista de Tareas</h2>
+      </div>
+      <CreateTaskForm folderId={folderId} onTaskCreated={fetchTasks} />
+      <TaskList
+        isScrollable={isScrollable}
+        isLoading={isLoading}
+        error={error}
+        hasTasks={hasTasks}
+        tasks={tasks}
+        fetchTasks={fetchTasks}
+      />
+    </div>
   );
-}
+};
 
-TaskManager.propTypes = {
+TaskList.propTypes = {
   folderId: PropTypes.string.isRequired,
 };
 
