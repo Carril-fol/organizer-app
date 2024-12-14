73fbf520e47845b7efeaf4ec34fd10367c89ecf3
Folco Carril <107985251+Carril-fol@users.noreply.github.com>
2024-12-13 22:32:08 -0300

    chore: Removed coments in code

--- backend/src/asgi.py
+++ backend/src/asgi.py
@@ -4,6 +4,6 @@ from hypercorn.asyncio import serve
 
 def start_server(app, mode="wsgi"):
     config = Config()
-    config.bind = ["0.0.0.0:8000"]  # Escuchar en todas las interfaces en el puerto 8000
+    config.bind = ["0.0.0.0:8000"]
     config.use_reloader = False
     asyncio.run(serve(app, config))
