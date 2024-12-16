07112226b6405113f38a84a5fb19c6c9b509eaa3
Folco Carril <107985251+Carril-fol@users.noreply.github.com>
2024-12-16 19:24:33 -0300

    chore: Removed URL backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,32 +1,67 @@
 import axios from "axios";
 import { refreshTokens } from "./authServices";
 
-const API = axios.create(
-  {
-    baseURL: "https://restful-organizer.onrender.com",
-    withCredentials: true,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  },
-);
+const urlBackend = import.meta.env.BACKEND_URL;
 
+const API = axios.create({
+  baseURL: urlBackend,
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+let isRefreshing = false;
+let failedQueue = [];
+
+const processQueue = (error, token = null) => {
+  failedQueue.forEach(prom => {
+    if (token) {
+      prom.resolve(token);
+    } else {
+      prom.reject(error);
+    }
+  });
+
+  failedQueue = [];
+};
 
 API.interceptors.response.use(
   (response) => {
     return response;
   },
   async (error) => {
+    const originalRequest = error.config;
+
     if (error.response && error.response.status === 401) {
+      // Si ya hay un intento de refrescar el token, se pone en cola la solicitud
+      if (isRefreshing) {
+        return new Promise((resolve, reject) => {
+          failedQueue.push({
+            resolve: (token) => {
+              resolve(API(originalRequest)); // Reintenta la solicitud original con el nuevo token
+            },
+            reject: (err) => reject(err),
+          });
+        });
+      }
+
+      // Si no se está refrescando el token, entonces hacerlo
+      isRefreshing = true;
+
       try {
-        await refreshTokens();
-        const originalRequest = error.config;
-        return API(originalRequest);
+        const newTokens = await refreshTokens(); // Refresca el token
+        processQueue(null, newTokens.accessToken); // Procesa la cola de solicitudes fallidas
+        return API(originalRequest); // Reintenta la solicitud original
       } catch (refreshError) {
+        processQueue(refreshError, null);
         console.error("Error al refrescar el token:", refreshError);
         return Promise.reject(refreshError);
+      } finally {
+        isRefreshing = false;
       }
     }
+
     return Promise.reject(error);
   }
 );
