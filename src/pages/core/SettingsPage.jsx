d6c59e6114067aa3309cb572579017cb31943a27
Folco Carril <107985251+Carril-fol@users.noreply.github.com>
2024-12-24 05:59:26 -0300

    feat: Added suspense in the imports from the components

--- src/pages/core/SettingsPage.jsx
+++ src/pages/core/SettingsPage.jsx
@@ -1,13 +1,19 @@
-import LoggedNavbar from "../../components/common/LoggedNavbar";
-import SettingsMenu from "../../components/core/SettingsMenu";
+import React, { Suspense } from "react";
+
+const LoggedNavbar = React.lazy(() => import("../../components/common/LoggedNavbar"));
+const SettingsMenu = React.lazy(() => import("../../components/core/SettingsMenu"));
 
 const SettingsPage = () => {
   return (
     <div className="flex flex-col">
-      <LoggedNavbar />
+      <Suspense fallback={<div>Cargando...</div>}>
+        <LoggedNavbar />
+      </Suspense>
       <main className="bg-gray-50">
         <section>
-          <SettingsMenu />
+          <Suspense fallback={<div>Cargando...</div>}>
+            <SettingsMenu />
+          </Suspense>
         </section>
       </main>
     </div>
