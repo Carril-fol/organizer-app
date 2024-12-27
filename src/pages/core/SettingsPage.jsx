5d1a1914915df1e26b40f790503fbc2313fa9828
Folco Carril <107985251+Carril-fol@users.noreply.github.com>
2024-12-27 03:08:32 -0300

    feat: Added section in page and form

--- src/pages/core/SettingsPage.jsx
+++ src/pages/core/SettingsPage.jsx
@@ -1,18 +1,18 @@
 import React, { Suspense } from "react";
 
 const LoggedNavbar = React.lazy(() => import("../../components/common/LoggedNavbar"));
-const SettingsMenu = React.lazy(() => import("../../components/core/SettingsMenu"));
+const SettingsForm = React.lazy(() => import("../../components/core/SettingsForm"));
 
 const SettingsPage = () => {
   return (
     <div className="flex flex-col">
-      <Suspense fallback={<div>Cargando...</div>}>
+      <Suspense fallback={<div></div>}>
         <LoggedNavbar />
       </Suspense>
       <main className="bg-gray-50">
-        <section>
-          <Suspense fallback={<div>Cargando...</div>}>
-            <SettingsMenu />
+        <section className="min-w-screen min-h-screen flex justify-center items-center">
+          <Suspense fallback={<div></div>}>
+            <SettingsForm />
           </Suspense>
         </section>
       </main>
