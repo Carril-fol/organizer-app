879d0e39d43c086745974530baa6d93a02290126
Folco Carril <107985251+Carril-fol@users.noreply.github.com>
2024-12-29 20:25:30 -0300

    chore: Separate logic from the component main in folder lists

--- src/components/folders/FolderLists.jsx
+++ src/components/folders/FolderLists.jsx
@@ -1,18 +1,23 @@
+import React, { useCallback } from "react";
 import PropTypes from "prop-types";
 import { FolderItem } from "./FolderItem";
 
 const FolderList = ({ folders, onFolderSelect, fetchFolders }) => {
 
+  const renderFolderItems = useCallback(() => {
+    return folders.map((folder) => (
+      <FolderItem
+        key={folder._id}
+        folder={folder}
+        fetchFolders={fetchFolders}
+        onFolderSelect={onFolderSelect}
+      />
+    ));
+  }, [folders, fetchFolders, onFolderSelect]);
+
   return (
     <div className="space-y-4 max-h-64 overflow-y-auto">
-      {folders.map((folder) => (
-        <FolderItem
-          key={folder._id}
-          folder={folder}
-          fetchFolders={fetchFolders}
-          onFolderSelect={onFolderSelect}
-        />
-      ))}
+      {renderFolderItems()}
     </div>
   );
 };
