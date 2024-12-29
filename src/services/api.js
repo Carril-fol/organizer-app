6f421da50f18fa8654086c13e3dfed7ec6aac89f
Folco Carril <107985251+Carril-fol@users.noreply.github.com>
2024-12-29 20:57:49 -0300

    chore: Added a interceptor in conection with the API

--- src/services/api.js
+++ src/services/api.js
@@ -11,58 +11,18 @@ const API = axios.create({
   },
 });
 
-let isRefreshing = false;
-let failedQueue = [];
-
-const processQueue = (error, token = null) => {
-  failedQueue.forEach(prom => {
-    if (token) {
-      prom.resolve(token);
-    } else {
-      prom.reject(error);
-    }
-  });
-
-  failedQueue = [];
-};
-
 API.interceptors.response.use(
-  (response) => {
-    return response;
-  },
+  (response) => response,
   async (error) => {
-    const originalRequest = error.config;
-
     if (error.response && error.response.status === 401) {
-      // Si ya hay un intento de refrescar el token, se pone en cola la solicitud
-      if (isRefreshing) {
-        return new Promise((resolve, reject) => {
-          failedQueue.push({
-            resolve: (token) => {
-              resolve(API(originalRequest)); // Reintenta la solicitud original con el nuevo token
-            },
-            reject: (err) => reject(err),
-          });
-        });
-      }
-
-      // Si no se está refrescando el token, entonces hacerlo
-      isRefreshing = true;
-
       try {
-        const newTokens = await refreshTokens(); // Refresca el token
-        processQueue(null, newTokens.accessToken); // Procesa la cola de solicitudes fallidas
-        return API(originalRequest); // Reintenta la solicitud original
+        refreshTokens();
+        const originalRequest = error.config;
+        return API(originalRequest);
       } catch (refreshError) {
-        processQueue(refreshError, null);
-        console.error("Error al refrescar el token:", refreshError);
         return Promise.reject(refreshError);
-      } finally {
-        isRefreshing = false;
       }
     }
-
-    return Promise.reject(error);
   }
 );
 
